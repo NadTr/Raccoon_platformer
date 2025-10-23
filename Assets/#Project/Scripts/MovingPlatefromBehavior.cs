3102c1938eeea13ddffeebe472980c5e194b26c4
NadTr <trauflernadine@gmail.com>
2025-10-23 11:35:15 +0200

    change game to use tilemap

--- Assets/#Project/Scripts/MovingPlatefromBehavior.cs
+++ Assets/#Project/Scripts/MovingPlatefromBehavior.cs
@@ -9,7 +9,7 @@ public class MovingPlatefromBehavior : MonoBehaviour
     {
         Move();
 
-        Vector3 origin = transform.position + Vector3.up * 0.4f + (goRight ? 2.5f : -0.5f) * Vector3.right * 1f;
+        Vector3 origin = transform.position + Vector3.up * 0.4f + (goRight ? 3.5f : -0.5f) * Vector3.right * 1f;
         Vector3 direction = (goRight ? 1f : -1f) * Vector3.right;
         RaycastHit2D sideHit = Physics2D.Raycast(origin, direction, 0.02f);
         Debug.DrawRay(origin, direction, Color.cyan);
